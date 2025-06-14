82b228fe3da846fa96c55855876d5c8dc086d963
Mark <Markyriott@gmail.com>
2025-06-14 19:59:35 -0400

    Advanced Stack Problem Added

--- codePath/Unit3/Session1/AdvancedA/p1.py
+++ codePath/Unit3/Session1/AdvancedA/p1.py
@@ -1,9 +1,26 @@
 def arrange_guest_arrival_order(arrival_pattern):
     stack = []
+    result = [''] * (len(arrival_pattern) + 1)
+    count = 1
 
     for i in range(len(arrival_pattern)):
         stack.append(i)
         if arrival_pattern[i] == "I":
+            while stack:
+                temp = stack.pop()
+                result[temp] = str(count)
+                count+= 1
+
+    stack.append(len(arrival_pattern))
+    
+    while stack:
+        temp = stack.pop()
+        result[temp] = str(count)
+        count+= 1
+
+    return result
+        
+
 
 
     
