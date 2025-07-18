e5bfe9377d8ae6e0e94a4052a77d6cc410c5b273
Mark <208412620+Markyriott@users.noreply.github.com>
2025-07-18 00:54:55 -0400

    fixed edge case for unit7/session2/standarda/p3

--- codePath/Unit7/Session2/StandardA/p3.py
+++ codePath/Unit7/Session2/StandardA/p3.py
@@ -1,24 +1,25 @@
 def count_checked_in_passengers(rooms):
 
-    def binary_search(rooms,left,right):
+    def binary_search(rooms,left,right,first):
         if left > right:
-            return 0
+            return len(rooms[first:])
         
-        mid = left + right // 2
+        mid = left + (right-left) // 2
 
         if rooms[mid] == 1:
-            return len(rooms[mid:])
+            first = mid
+            right = mid - 1
+            return binary_search(rooms,left,right,first)
         elif rooms[mid] == 0:
             left = mid + 1
-            return binary_search(rooms,left,right)
+            return binary_search(rooms,left,right,first)
         
-    return binary_search(rooms,0 ,len(rooms)- 1)
-
+    return binary_search(rooms,0 ,len(rooms)- 1,len(rooms))
 
 rooms1 = [0, 0, 0, 1, 1, 1, 1]
 rooms2 = [0, 0, 0, 0, 0, 1]
 rooms3 = [0, 0, 0, 0, 0, 0]
-
+#rooms3 = [0,1,1,1,1,1,1,1,1]
 print(count_checked_in_passengers(rooms1)) 
 print(count_checked_in_passengers(rooms2))
 print(count_checked_in_passengers(rooms3))
