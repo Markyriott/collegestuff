67164a9d7909f8261f7bfe2556357f8ab23b104a
Mark <208412620+Markyriott@users.noreply.github.com>
2025-07-28 20:27:36 -0400

    Added all problems to unit8/session2/standardA

---
+++ codePath/unit8/Session2/StandardA/p6.py
@@ -0,0 +1,68 @@
+from collections import deque 
+
+# Tree Node class
+class TreeNode:
+  def __init__(self, value, key=None, left=None, right=None):
+      self.key = key
+      self.val = value
+      self.left = left
+      self.right = right
+
+def build_tree(values):
+  if not values:
+      return None
+
+  def get_key_value(item):
+      if isinstance(item, tuple):
+          return item[0], item[1]
+      else:
+          return None, item
+
+  key, value = get_key_value(values[0])
+  root = TreeNode(value, key)
+  queue = deque([root])
+  index = 1
+
+  while queue:
+      node = queue.popleft()
+      if index < len(values) and values[index] is not None:
+          left_key, left_value = get_key_value(values[index])
+          node.left = TreeNode(left_value, left_key)
+          queue.append(node.left)
+      index += 1
+      if index < len(values) and values[index] is not None:
+          right_key, right_value = get_key_value(values[index])
+          node.right = TreeNode(right_value, right_key)
+          queue.append(node.right)
+      index += 1
+
+  return root
+
+class TreeNode:
+    def __init__(self, key, val, left=None, right=None):
+        self.key = key      # Plant price
+        self.val = val      # Plant name
+        self.left = left
+        self.right = right
+def pick_plant(root,budget):
+    def search(node, target):
+        if not node:
+            return None
+        if node.val < target:
+            right = search(node.right,target)
+            rightval = right.val if right else 0
+            closest = node if node.val > rightval else right
+            return closest
+        else:
+            return search(node.left,target)
+    
+    res = search(root,budget)
+    return res.key if res else None			
+
+values = [(50, "Fiddle Leaf Fig"), (25, "Monstera"), (70, "Snake Plant"), (15, "Aloe"), 
+            (40, "Pothos"), (60, "Fern"), (80, "ZZ Plant")]
+inventory = build_tree(values)
+
+print(pick_plant(inventory, 50)) 
+print(pick_plant(inventory, 25)) 
+print(pick_plant(inventory, 15)) 
