9c393a8f21f3e47d5512985590e910c282ad4c74
Mark <208412620+Markyriott@users.noreply.github.com>
2026-02-05 19:58:41 -0500

    Finished CIS3140 Homework 1

--- CIS3140/Homework1.scm
+++ CIS3140/Homework1.scm
@@ -1,23 +1,47 @@
-;calculator
-
-(define (calc op n1 n2)
-  (cond 
-    [(string=? op "add") (+ n1 n2)]
-    [(string=? op "subtract") (- n1 n2)]
-    [(string=? op "divide") (/ n1 n2)]
-    [(string=? op "multiply") (* n1 n2)]
+;1. 4 function calculator
+(define calc 
+  (lambda (op n1 n2)
+    (cond 
+      ((string=? op "add") (+ n1 n2))
+      ((string=? op "subtract") (- n1 n2))
+      ((string=? op "divide") (/ n1 n2))
+      ((string=? op "multiply") (* n1 n2))
+      )
     )
   )
 
-(display (calc "add" 5 6))
+(display (calc "add" 5 6)) ; output: 11
 (newline)
-(display (calc "subtract" 5 6))
+(display (calc "subtract" 5 6)) ; output: -1
 (newline)
-(display (calc "divide" 6 2))
+(display (calc "divide" 6 2)) ; output: 3
 (newline)
-(display (calc "multiply" 5 5))
+(display (calc "multiply" 5 5)) ; output: 25
 (newline)
 
-;reverse a list
+; 2. reverse a list
+(define reverse 
+  (lambda (lst) 
+    (if (equal? lst (list)) (list) 
+      (append (reverse (cdr lst)) (list (car lst)))
+      )
+    )
+  )
 
-;count occurences of "e"
+(display (reverse (list 1 2 3 4 5))) ; output: (5 4 3 2 1)
+(newline)
+
+
+; 3. count occurences of "e"
+(define count
+  (lambda (lst)
+    (if (equal? lst (list)) 0
+      (+ (if (equal? (car lst) "e") 1 0) (count(cdr lst)))
+      )
+    )
+  )
+
+(display (count (list "e" "e" "a" "e"))) ; output: 3
+(newline)
+(display (count (list "e" "e" "t" "s" "e" "f" "a" "e"))) ; output: 4
+(newline)
